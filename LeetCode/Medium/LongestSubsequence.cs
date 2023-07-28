965be0bf307f191f85b31ff9ff18072fa78c55cc
HennadiiHorbachov <genagorbachov1@gmail.com>
2023-07-29 00:09:07 +0300

    Solved LongestSubsequence problem

---
+++ LeetCode/Medium/LongestSubsequence.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Medium;
+
+// 1218 Longest Arithmetic Subsequence of Given Difference
+internal class LongestSubsequence
+{
+    public int Handle(int[] arr, int difference)
+    {
+        var dictionary = new Dictionary<int, int>();
+        int maxSequenceLength = 1;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int previous = arr[i] - difference;
+            if (dictionary.ContainsKey(previous))
+            {
+                int sequenceLength = dictionary[previous] + 1;
+                dictionary[arr[i]] = sequenceLength;
+                maxSequenceLength = Math.Max(maxSequenceLength, sequenceLength);
+            }
+            else
+            {
+                dictionary[arr[i]] = 1;
+            }
+        }
+
+        return maxSequenceLength;
+    }
+}
