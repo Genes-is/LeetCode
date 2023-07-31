e82f5c1c17ca14cdde59a34dba5075622d747272
HennadiiHorbachov <genagorbachov1@gmail.com>
2023-07-31 18:49:56 +0300

    Solved Candy problem

---
+++ LeetCode/Hard/Candy.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Hard;
+
+// 135 Candy
+internal class Candy
+{
+    public int Handle(int[] ratings)
+    {
+        int[] candyCounts = new int[ratings.Length];
+        candyCounts[0] = 1;
+
+        for (int i = 1; i < ratings.Length; i++)
+        {
+            if (ratings[i] > ratings[i - 1])
+            {
+                candyCounts[i] = candyCounts[i - 1] + 1;
+            }
+            else
+            {
+                candyCounts[i] = 1;
+            }
+        }
+
+        int totalCandyCount = candyCounts[^1];
+
+        for (int i = ratings.Length - 2; i >= 0; i--)
+        {
+            if (ratings[i] > ratings[i + 1] && candyCounts[i] <= candyCounts[i + 1])
+            {
+                candyCounts[i] = candyCounts[i + 1] + 1;
+            }
+
+            totalCandyCount += candyCounts[i];
+        }
+
+        return totalCandyCount;
+    }
+}
